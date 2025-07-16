151bd405e227c40cb6fdb7c02a3a4d734f05834e
Victor Pernet <v.pernet24@gmail.com>
2025-07-16 09:15:13 +0200

    script.js récupérer infos basiques

---
+++ script.js
@@ -0,0 +1,12 @@
+let divJeu = document.getElementById("divJeu")
+console.log(divJeu)
+
+let h2 = document.querySelector("#divJeu h2")
+console.log(h2)
+
+let listh2 = document.querySelectorAll("h2")
+console.log(listh2)
+
+for (let i = 0; i < listh2.length; i++) {
+    console.log(listh2[i])
+}
