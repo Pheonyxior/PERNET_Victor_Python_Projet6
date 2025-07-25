1c6470ef1b23dc340949e881740ca89aa3b50009
Victor PERNET <victor@av-connexion.fr>
2025-07-25 13:03:08 +0200

    unga javascript

--- script.js
+++ script.js
@@ -27,50 +27,17 @@ let homepage = "http://localhost:8000/api/v1/titles/"
 // * &rating_contains=
 // * 
 
-async function filter_movies_by(
-        year = "", min_year = "", max_year = "", imdb_score = "",
-        imdb_score_min = "", imdb_score_max, title = "", title_contains = "",
-        genre = "", genre_contains = "", sort_by = "", director = "", 
-        director_contains = "", writer = "", writer_contains = "", actor = "",
-        actor_contains = "", country = "", country_contains = "", lang = "",
-        lang_contains = "", company = "", company_contains = "", rating = "",
-        rating_contains = ""
-)
+async function get_best_movie()
 {
-    let url = `${homepage}`+
-            `?year=${year}`+
-            `&minyear=${min_year}`+
-            `&max_year=${max_year}`+
-            `&imdb_score=${imdb_score}`+
-            `&imdb_score_min=${imdb_score_min}`+
-            `&imdb_score_max=${imdb_score_max}`+
-            `&title=${title}`+
-            `&title_contains=${title_contains}`+
-            `&genre=${genre}`+
-            `&genre_contains=${genre_contains}`+
-            `&sort_by=${sort_by}`+
-            `&director=${director}`+
-            `&director_contains=${director_contains}`+
-            `&writer=${writer}`+
-            `&writer_contains=${writer_contains}`+
-            `&actor=${actor}`+
-            `&actor_contains=${actor_contains}`+
-            `&country=${country}`+
-            `&country_contains=${country_contains}`+
-            `&lang=${lang}`+
-            `&lang_contains=${lang_contains}`+
-            `&company=${company}`+
-            `&company_contains=${company_contains}`+
-            `&rating=${rating}`+
-            `&rating_contains=${rating_contains}`;
-    
-    // url.replace(/[\n\r]+/g, '');
-    // url.replace(/^\s+|\s+$/gm,'';
+    url = "http://localhost:8000/api/v1/titles/?sort_by=-imdb_score"
     console.log(url)
     let response = await fetch(url);
     if (response.ok){
-        let json = await response.json();
-        return json;
+        let data = await response.json();
+        console.log(data.results);
+        console.log(data.results[0])
+        console.log(data.results[0].url)
+        return data;
     }
     else {
         alert("HTTP-Error: " + response.status);
@@ -78,10 +45,11 @@ async function filter_movies_by(
     }
 }
 
-let json = filter_movies_by(sort_by="-imdb_score");
-if (json != null){
-    console.log(json)
-    console.log(json["results"]["title"]);
+let my_json = get_best_movie();
+if (my_json != null){
+    console.log(my_json)
+    console.log(my_json.results)
+    console.log(my_json["results"]);
 }
 
 // let divJeu = document.getElementById("divJeu")
