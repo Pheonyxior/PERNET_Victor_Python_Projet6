bf4266849de9a0af8acf5f835d24f3435e98bde2
Victor Pernet <v.pernet24@gmail.com>
2025-10-23 16:42:35 +0200

    99% fini

--- script.js
+++ script.js
@@ -127,8 +127,9 @@ async function set_movie_thumbnail(movie_id, category_s, num_s)
     // console.log("movie ", movie)
     let baliseImage = document.getElementById(category_s+"Img"+num_s);
     baliseImage.src = movie["image_url"];
+    // console.log(category_s+"Title"+num_s)
     let baliseTitle = document.getElementById(category_s+"Title"+num_s)
-    baliseTitle.innerText = movie["title"]
+    baliseTitle.innerText = movie["original_title"]
     // let baliseDesc = document.getElementById(category_s+"Desc"+num_s)
     // baliseDesc.innerText = best_movie["description"]
 }
@@ -211,12 +212,62 @@ async function set_other_movies_thumbnail(genre)
     }
 }
 
+async function set_altother_movies_thumbnail(genre)
+{
+    let movie_amount = 6
+    let movies = await get_movies(genre, movie_amount)
+
+    let btns = document.getElementsByClassName("altother-btn")
+
+    for (let i = 1; i<=movie_amount; i++)
+    {
+        let movie = movies[i-1]
+        // console.log("Movie ", movie)
+        set_movie_thumbnail(movie["id"], "altother", String(i))
+        let btn = btns[i-1]
+        btn.addEventListener('click', () => displayModal(movie["id"]))
+    }
+}
+
+
+function toggleDropDownVisibility(property) {
+    console.log("toggleDropDownVisibility "+property)
+    let visibility = document.documentElement.style.getPropertyValue(property)
+    console.log(visibility)
+    if (visibility === 'inline')
+    {
+        document.documentElement.style.setProperty(property, "none");
+    }
+    else
+    {
+        document.documentElement.style.setProperty(property, "inline");
+    }
+}
+
+function hideDropdowns() {
+    console.log("hideDropdowns")
+    document.documentElement.style.setProperty("--drop_down_visibility1", "none");
+    document.documentElement.style.setProperty("--drop_down_visibility2", "none");
+}
+
 async function updateOtherMovies(genre, index) 
 {
-    console.log("updateOtherMovies")
-    let displayedGenre = document.getElementById("displayedGenre"+index)
-    displayedGenre.innerText = genre
+    // console.log("updateOtherMovies " +genre+index)
+    toggleDropDownVisibility("--drop_down_visibility"+index)
+    let displayedGenre = document.getElementById("displayedGenre"+index).childNodes[0]
+    // console.log(displayedGenre)
+    displayedGenre.textContent = genre
     await set_other_movies_thumbnail(genre)
+}
+
+async function updateAltOtherMovies(genre, index) 
+{
+    // console.log("updateAltOtherMovies " +genre+index)
+    toggleDropDownVisibility("--drop_down_visibility"+index)
+    let displayedGenre = document.getElementById("displayedGenre"+index).childNodes[0]
+    // console.log(displayedGenre)
+    displayedGenre.textContent = genre
+    await set_altother_movies_thumbnail(genre)
 }
 
 function set_modal()
@@ -250,57 +301,107 @@ async function setModalContent(movie_id) {
     let details = await get_movie_details(movie_id)
     console.log(details)
     
+    let desktopthumbn = document.getElementById("desktopDetailsThumbn")
+    desktopthumbn.src = details["image_url"];
     let thumbn = document.getElementById("detailsThumbn")
     thumbn.src = details["image_url"];
 
     let title = document.getElementById("detailsTitle")
-    title.innerText = details["title"]
+    title.innerText = details["original_title"]
     
     let year = details["year"]
-    let genres = details["genres"]
+    let genres_list = details["genres"]
+    
+    let genres_str = ""
+    genres_list.forEach((genre) => {
+        genres_str += String(genre) + ", "
+    });
+    let genres = genres_str.replace(/, $/, "");
+    
     let pg = details["rated"]
+    console.log(details["rated"])
     let duration = details["duration"]
-    let countries = details["countries"]
+    
+    let countries_list = details["countries"]
+    let countries_str = ""
+    countries_list.forEach((country) => {
+        countries_str += String(country) + " / "
+    });
+    let countries = countries_str.replace(/ \/ $/, "");
+    
     let imdb = details["imdb_score"]
     let wwg_income = details["worldwide_gross_income"]
     
     let detailsDateGenre = document.getElementById("detailsDateGenre")
     detailsDateGenre.innerText = year + " - " + genres
     let detailsPGDurCountries = document.getElementById("detailsPGDurCountries")
-    if (pg.length > 3){
+    
+    console.log(pg)
+    if (pg.length > 3 || !pg || pg == 'PG'){
         pg = "Unknown"
     }
     detailsPGDurCountries.innerText = "PG-"+ pg + " - " + duration + " minutes (" + countries + ")"
+    
     let detailsIMDB = document.getElementById("detailsIMDB")
     detailsIMDB.innerText = "IMDB score: " + imdb + "/10"
     let detailsIncome = document.getElementById("detailsIncome")
     if (!wwg_income){
         wwg_income = "Unknown"
     }
+    else{
+        wwg_income = String(wwg_income)
+        d = wwg_income[5]
+        wwg_income = wwg_income.slice(0, 4)
+        wwg_income = "$" + wwg_income.slice(0, 4) + ',' + d + 'm'
+    }
     detailsIncome.innerText = "Recettes au box-office: " + wwg_income
 
     let directors_list = details["directors"]
     let directors_str = ""
     directors_list.forEach((director) => {
-        directors_str += String(director)
+        directors_str += String(director) + ", "
     });
+    directors_str = directors_str.replace(/, $/, ".");
     let directors = document.getElementById("detailsDirectors")
-    // directors.innerText = directors_str
-    directors.innerText = details["directors"]
+    directors.innerText = directors_str
     
     let desc = document.getElementById("detailsDesc")
-    desc.innerText = details["description"]
+    desc.innerText = details["long_description"]
     
+    let actors_list = details["actors"]
+    let actors_str = ""
+    actors_list.forEach((actor) => {
+        actors_str += String(actor) + ", "
+    });
+    actors_str = actors_str.replace(/, $/, ".");
     let actors = document.getElementById("detailsActors")
-    actors.innerText = details["actors"]
+    actors.innerText = actors_str
+}
+
+function setUnfocusClick(){
+    document.addEventListener("click", (event) => {
+    const el = event.target;
+    if (
+        el.tagName === "BUTTON" ||
+        el.tagName === "A" ||
+        el.tagName === "INPUT" ||
+        el.tagName === "TEXTAREA" ||
+        el.tagName === "IMG" ||
+        el.isContentEditable
+    ) {
+        return; // skip interactive elements
+    }
+    hideDropdowns()
+    });
 }
 
 
-
+setUnfocusClick()
 set_best_movies_thumbnail()
 set_myst_movies_thumbnail()
 set_anim_movies_thumbnail()
-set_other_movies_thumbnail("adventure")
+set_other_movies_thumbnail("Action")
+set_altother_movies_thumbnail("Sci-Fi")
 
 set_modal()
 
